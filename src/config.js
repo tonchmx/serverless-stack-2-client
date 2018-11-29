28701e9e2777c95b03ffc5d8f9bac7049291a5c2
Toño Cruz <tonchmx@gmail.com>
2018-11-29 11:43:00 -0600

    Adding Stripe keys to config

--- src/config.js
+++ src/config.js
@@ -12,7 +12,8 @@ const dev = {
     USER_POOL_ID: "us-east-1_D7hFGadQS",
     APP_CLIENT_ID: "ba4l9ior7a9rlpssamacc6513",
     IDENTITY_POOL_ID: "us-east-1:d8a1908f-a1de-4fff-9d5e-892265470217"
-  }
+  },
+  STRIPE_KEY: "pk_test_dVOG3KTCkmF2TtAZOOsGxDxk"
 };
 
 const prod = {
@@ -29,7 +30,8 @@ const prod = {
     USER_POOL_ID: "us-east-1_1hLYqRxiN",
     APP_CLIENT_ID: "4n1499m79ush2sbpljq3dn9ut5",
     IDENTITY_POOL_ID: "us-east-1:32a590f8-06ed-4dcf-8f8a-1bba81187a31"
-  }
+  },
+  STRIPE_KEY: "pk_test_dVOG3KTCkmF2TtAZOOsGxDxk"
 };
 
 // Default to dev if not set
